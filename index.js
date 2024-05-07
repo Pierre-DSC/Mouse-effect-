009918e54fd2adc94f032c54715296d1d0929114
Pierre <descormes.pierre22@gmail.com>
2024-05-07 22:55:49 +0200

    finalised

--- index.js
+++ index.js
@@ -6,18 +6,24 @@
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
 
-document.addEventListener('mousemove', (e) => {
-  const customCursor = document.querySelector('#cursor');
-  customCursor.style.top = e.clientY + "px";
-  customCursor.style.left = e.clientX + "px";
-});
-document.addEventListener('mousemove', (e) => {
-  const customCursor = document.querySelector('#mouse1');
-  customCursor.style.top = e.clientY + "px";
-  customCursor.style.left = e.clientX + "px";
-});
-document.addEventListener('mousemove', (e) => {
-  const customCursor = document.querySelector('#mouse2');
-  customCursor.style.top = e.clientY + "px";
-  customCursor.style.left = e.clientX + "px";
+// window.addEventListener('mousemove', (e) => {
+//   cursor.style.top = e.clientY + "px";
+//   cursor.style.left = e.clientX + "px";
+// });
+// window.addEventListener('mousemove', (e) => {
+//   mouse1.style.top = e.clientY + "px";
+//   mouse1.style.left = e.clientX + "px";
+// });
+// window.addEventListener('mousemove', (e) => {
+//   mouse2.style.top = e.clientY + "px";
+//   mouse2.style.left = e.clientX + "px";
+// });
+
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.clientY + "px";
+    mouse.style.left = e.clientX + "px";
+  });
 });
